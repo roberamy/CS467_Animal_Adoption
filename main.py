cdb7c6f2f341bab321837ab4524774f7cf701a56
Amy Robertson <49350741+roberamy@users.noreply.github.com>
2021-01-23 16:08:43 -0800

    Basic main.py, admin template
    Admin tabular view w/linked add new profile & update profile html templates. Navbar template included as well.

---
+++ main.py
@@ -0,0 +1,29 @@
+
+
+import os
+
+from flask import Flask, render_template, request, Response
+
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def root():
+    return render_template('index.html')
+
+@app.route("/admin_profiles", methods=["GET"])
+def admin_profiles():
+    return render_template('admin_profiles.html')
+
+@app.route("/add_profile", methods=["GET"])
+def add_profile():
+    return render_template('add_profile.html')
+    
+@app.route("/update_profile", methods=["GET"])
+def update_profile():
+    return render_template('update_profile.html')
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
