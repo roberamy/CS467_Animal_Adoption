903f18d58232ff7430013b59986fe758456940cc
Amy Robertson <49350741+roberamy@users.noreply.github.com>
2021-01-27 09:37:25 -0800

    Correction to OAuth
    Ignore the second set of Oauth info. I'm using my own GAE setup for local testing.

--- OAuth.py
+++ OAuth.py
@@ -26,19 +26,19 @@ from datetime import datetime
 bp = Blueprint('OAuth', __name__)
 client = datastore.Client()
 
-#CLIENT_ID = r'939115278036-he2m51te7ohrp1m9r457nos1dbnh5u2o.apps.googleusercontent.com'
-#CLIENT_SECRET = r'LQQ_RyrsV-eA1uiuux0RrI7J'
-#SCOPES = ['openid', 'https://www.googleapis.com/auth/userinfo.email',
-#    'https://www.googleapis.com/auth/userinfo.profile']
-#REDIRECT_URI = 'https://datingappforanimaladoption.wl.r.appspot.com/authorization'
-
-# TESTING LOCALLY w/my GAE app setup (Amy)
-CLIENT_ID = r'962694165859-mvk5ndmuto54p713jl623611ifamlugu.apps.googleusercontent.com'
-CLIENT_SECRET = r'JgEZabSFKK2vDXWC9AeUty6D'
+CLIENT_ID = r'939115278036-he2m51te7ohrp1m9r457nos1dbnh5u2o.apps.googleusercontent.com'
+CLIENT_SECRET = r'LQQ_RyrsV-eA1uiuux0RrI7J'
 SCOPES = ['openid', 'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/userinfo.profile']
+REDIRECT_URI = 'https://datingappforanimaladoption.wl.r.appspot.com/authorization'
+
+# TESTING LOCALLY w/my GAE app setup (Amy)
+#CLIENT_ID = r'962694165859-mvk5ndmuto54p713jl623611ifamlugu.apps.googleusercontent.com'
+#CLIENT_SECRET = r'JgEZabSFKK2vDXWC9AeUty6D'
+#SCOPES = ['openid', 'https://www.googleapis.com/auth/userinfo.email',
+#    'https://www.googleapis.com/auth/userinfo.profile']
 #REDIRECT_URI = 'https://roberamy-animal-app.wl.r.appspot.com/authorization'
-REDIRECT_URI = 'http://localhost:8080/authorization'
+#REDIRECT_URI = 'http://localhost:8080/authorization'
 
 OAUTH = OAuth2Session(CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPES)
     
