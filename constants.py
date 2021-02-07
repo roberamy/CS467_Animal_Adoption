409f101db97d809d124844a0dc02c4483ac2586a
Jasper Wong <34344982+mrLennyboy@users.noreply.github.com>
2021-02-07 08:56:30 -0800

    test comments

--- constants.py
+++ constants.py
@@ -9,19 +9,21 @@ pets = "pets"
 #url = 'https://datingappforanimaladoption.wl.r.appspot.com'
 
 # TESTING LOCALLY w/my GAE app setup (Amy)
-CLIENT_ID = r'962694165859-mvk5ndmuto54p713jl623611ifamlugu.apps.googleusercontent.com'
-CLIENT_SECRET = r'JgEZabSFKK2vDXWC9AeUty6D'
-SCOPES = ['openid', 'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/userinfo.profile']
-REDIRECT_URI = 'https://roberamy-animal-app.wl.r.appspot.com/authorization'
-#REDIRECT_URI = 'http://localhost:8080/authorization'
-url = 'https://roberamy-animal-app.wl.r.appspot.com'
-#url = 'http://localhost:8080'
+# CLIENT_ID = r'962694165859-mvk5ndmuto54p713jl623611ifamlugu.apps.googleusercontent.com'
+# CLIENT_SECRET = r'JgEZabSFKK2vDXWC9AeUty6D'
+# SCOPES = ['openid', 'https://www.googleapis.com/auth/userinfo.email',
+#     'https://www.googleapis.com/auth/userinfo.profile']
+# # REDIRECT_URI = 'https://roberamy-animal-app.wl.r.appspot.com/authorization'
+# REDIRECT_URI = 'http://localhost:8080/authorization'
+# # url = 'https://roberamy-animal-app.wl.r.appspot.com'
+# url = 'http://localhost:8080'
 
 # TESTING LOCALLY w/my GAE app setup (Jasper)
-# CLIENT_ID = r'20872689223-stjkrofc8280dtpnghpinqfif2dt7sqg.apps.googleusercontent.com'
-# CLIENT_SECRET = r'LUKL4Udr-T3Pki4lhUgZP32J'
-# SCOPES = ['openid', 'https://www.googleapis.com/auth/userinfo.email',
-#    'https://www.googleapis.com/auth/userinfo.profile']
-#  REDIRECT_URI = 'https://wongjasp-animal-app.wl.r.appspot.com/authorization'
-#  REDIRECT_URI = 'http://localhost:8080/authorization'
+CLIENT_ID = r'20872689223-stjkrofc8280dtpnghpinqfif2dt7sqg.apps.googleusercontent.com'
+CLIENT_SECRET = r'LUKL4Udr-T3Pki4lhUgZP32J'
+SCOPES = ['openid', 'https://www.googleapis.com/auth/userinfo.email',
+   'https://www.googleapis.com/auth/userinfo.profile']
+# REDIRECT_URI = 'https://wongjasp-animal-app.wl.r.appspot.com/authorization'
+# url = 'https://wongjasp-animal-app.wl.r.appspot.com'
+REDIRECT_URI = 'http://localhost:8080/authorization'
+url = 'http://localhost:8080'
