ff783251623a5b044ea00d8e260033275ceefd4b
Jasper Wong <34344982+mrLennyboy@users.noreply.github.com>
2021-02-07 17:45:47 -0800

    Update profiles.py

---
+++ profiles.py
@@ -0,0 +1,15 @@
+###############################################################################################################
+#
+# Author: Gregory A. Bauer, Jasper Wong, Amy Robertson
+# Email: bauergr@oregonstate.edu
+# Course: CS467_400_W2021
+#
+# Description: Routes for profiles page. Profile card dynamic.
+#
+# Note:
+#
+#
+# References:
+# 
+###############################################################################################################
+
