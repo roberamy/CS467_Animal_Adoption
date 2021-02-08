173e583e79db11cf584dae2e00c6bda6ccc4a33d
Jasper Wong <34344982+mrLennyboy@users.noreply.github.com>
2021-02-07 21:17:06 -0800

    profiles html and py jinja test

--- profiles.py
+++ profiles.py
@@ -30,6 +30,29 @@ client = datastore.Client()
 from OAuth import printSession
 
 ###############################################################################################################
+@bp.route('/profiles', methods=["GET"])
+def view_profile():
+    # printSession('***** ADOPT *****')
+    if 'sub' not in session:
+        return "Error: \'sub\' not in session."
+    else:
+        # Direct requests to GAE database
+        if request.method == 'GET':
+            JWT = session['id_token']
+            # Grab 'sub' ID from JWT verification
+            req = requests.Request()
+            # Raises: exceptions.GoogleAuthError – If the issuer is invalid.
+            id_info = id_token.verify_oauth2_token(JWT, req, constants.CLIENT_ID)
+            if session['sub'] != id_info['sub']:
+                return 'Error": "Invalid JWT'
+            # Get all pets from the datastore owned by user
+            query = client.query(kind=constants.pets)
+            profiles = list(query.fetch())
+            for r in profiles:
+                r['id'] = r.key.id
+                r['self'] = constants.url + '/pets/' + str(r.key.id)
+            return render_template('profiles.html', profiles=profiles)
+        return render_template('Real response message')
 
 # @bp.route('/admin', methods=['GET'])
 # def adminPage():
