b0143631ddcd1940f9ee4fc759736683a212a974
Jasper Wong <34344982+mrLennyboy@users.noreply.github.com>
2021-02-10 14:48:11 -0800

    Update profiles.py

--- profiles.py
+++ profiles.py
@@ -37,32 +37,13 @@ from OAuth import printSession
 ###############################################################################################################
 @bp.route('/profiles', methods=["GET"])
 def view_profile():
-    # printSession('***** ADOPT *****')
     if 'sub' not in session:
         return "Error: \'sub\' not in session!!!"
     else:
         # Direct requests to GAE database
         if request.method == 'GET':
-            JWT = session['id_token']
-            # Grab 'sub' ID from JWT verification
-            req = requests.Request()
-            # Raises: exceptions.GoogleAuthError – If the issuer is invalid.
-            id_info = id_token.verify_oauth2_token(JWT, req, constants.CLIENT_ID)
-            if session['sub'] != id_info['sub']:
-                return 'Error": "Invalid JWT'
-            # Get all pets from the datastore owned by user
-            query = client.query(kind=constants.pets)
-            profiles = list(query.fetch())
-
-            profile_img = []
-            for r in profiles:
-                r['id'] = r.key.id
-                r['self'] = constants.url + '/pets/' + str(r.key.id)
-                # temp url, file name needs to be from dogs assets file and remove numbers
-                r['img'] = str(r['name']) + "/" + str(r['name']) + "-01.jpeg"
-                profile_img.append(r['img'])
-                # print(r)
-                # print(r['img'])
-                # print(profile_img)
-            return render_template('profiles.html', profiles=profiles, profile_img=profile_img)
+            # Return all pet entities in the datastore to populate 'admin_profiles.html'
+            # Instantiate singleton PetDsRepository class with member functions -- see 'repository.py'
+            data = PetDsRepository.all()
+            return render_template('profiles.html', pets=data)
         return render_template('Real response message')
