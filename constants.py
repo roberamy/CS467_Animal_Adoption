7da6fc8381d5bc4cb42e6103cf4287056ce4c731
Jasper Wong <34344982+mrLennyboy@users.noreply.github.com>
2021-02-14 16:42:27 -0800

    Revert "Merge branch 'MLB--pet_profile_dos' of https://github.com/roberamy/CS467_Animal_Adoption into MLB--pet_profile_dos"
    This reverts commit 56b9b20cd186bba5fa641011758dc9ade352fa7a, reversing
    changes made to bcac6a6d8022448472cd4f82c524a983c0936257.

--- constants.py
+++ constants.py
@@ -1,14 +1,11 @@
 users = "users"
 pets = "pets"
-breeds = "breeds"
 
 CLIENT_ID = r'939115278036-he2m51te7ohrp1m9r457nos1dbnh5u2o.apps.googleusercontent.com'
 CLIENT_SECRET = r'LQQ_RyrsV-eA1uiuux0RrI7J'
 SCOPES = ['openid', 'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/userinfo.profile']
-
-#REDIRECT_URI = 'https://datingappforanimaladoption.wl.r.appspot.com/authorization'
+# REDIRECT_URI = 'https://datingappforanimaladoption.wl.r.appspot.com/authorization'
 REDIRECT_URI = 'http://localhost:8080/authorization'
-#url = 'https://datingappforanimaladoption.wl.r.appspot.com/'
-
+# url = 'https://datingappforanimaladoption.wl.r.appspot.com/'
 url = 'http://localhost:8080'
