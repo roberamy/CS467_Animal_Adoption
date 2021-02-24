969c9fa2496394b8e281bb3ff236b63ad5e3cbea
Jasper Wong <34344982+mrLennyboy@users.noreply.github.com>
2021-02-23 21:48:38 -0800

    integrate filter code to adopt

--- adopt.py
+++ adopt.py
@@ -36,6 +36,11 @@ from flask_paginate import Pagination, get_page_args
 # User modules
 from repository import *
 
+# Used for /profiles route
+species = "Any"
+breed = "Any" 
+pdata = PetDsRepository.all()
+
 #import requests
 bp = Blueprint('adopt', __name__)
 client = datastore.Client()
@@ -57,62 +62,106 @@ def view_pet_page(pet_id):
         # redo temporary error response
         return "Error"
 
-###############################################################################
+# # ###############################################################################
+# # helper pagination function, per_page modified in __init__py
+# def get_pet_page(data, offset=0, per_page=10):
+#     return data[offset: offset + per_page]
+
+# @bp.route('/adopt_profiles', methods=["GET"])
+# def view_profile():
+#     printSession('***** Adopt_profiles *****')
+#     if 'sub' not in session:
+#         return "sub not in session."
+#     elif request.method == 'GET':
+#         # Return all pet entities in the datastore to populate 'profiles.html'
+#         # Instantiate singleton PetDsRepository class with member functions -- see 'repository.py'
+#         data = PetDsRepository.all()
+
+#         total = len(data)
+#         # pagination code
+#         page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
+        
+#         # change per page value after get_page_args
+#         per_page = 9
+#         offset = (page - 1) * per_page
+
+#         pagination_adopt_profile = get_pet_page(data, offset=offset, per_page=per_page)
+
+#         pagination = Pagination(page=page, 
+#                                 per_page=per_page, 
+#                                 total=total,
+#                                 css_framework='bootstrap4')      
+
+#         return render_template('adopt_profiles.html',
+#                                pets=pagination_adopt_profile,
+#                                page=page,
+#                                per_page=per_page,
+#                                pagination=pagination)
+        
+#         # API Link accessing public data format https://storage.googleapis.com/BUCKET_NAME/OBJECT_NAME
+#     else:
+#         # redo temporary error response
+#         return "Error Error"
+
 # helper pagination function, per_page modified in __init__py
 def get_pet_page(data, offset=0, per_page=10):
     return data[offset: offset + per_page]
 
-@bp.route('/adopt_profiles', methods=["GET"])
+# ################# Greg implementation integrated
+@bp.route('/adopt_profiles', methods=["GET", "POST"])
 def view_profile():
-    printSession('***** Adopt_profiles *****')
+    global species, breed, pdata
     if 'sub' not in session:
         return "sub not in session."
-    elif request.method == 'GET':
-        # Return all pet entities in the datastore to populate 'profiles.html'
-        # Instantiate singleton PetDsRepository class with member functions -- see 'repository.py'
-        data = PetDsRepository.all()
+    else:
+        if request.method == 'POST':
+            content = request.get_json()
+            species = content['species']
+            breed = content['breed']
+            if species == 'Any' and breed == "Any":
+                pdata = PetDsRepository.all()    
+        else:
+            if species == 'Any' and breed == "Any":
+                pdata = PetDsRepository.all() 
+            else:
+                pdata = PetDsRepository.filter(species,breed)
 
-        total = len(data)
+        total = len(pdata)
         # pagination code
         page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-        
+
         # change per page value after get_page_args
         per_page = 9
         offset = (page - 1) * per_page
 
-        pagination_adopt_profile = get_pet_page(data, offset=offset, per_page=per_page)
+        pagination_adopt_profile = get_pet_page(pdata, offset=offset, per_page=per_page)
 
         pagination = Pagination(page=page, 
                                 per_page=per_page, 
                                 total=total,
-                                css_framework='bootstrap4')
+                                css_framework='bootstrap4'
+                                )      
 
         return render_template('adopt_profiles.html',
                                pets=pagination_adopt_profile,
                                page=page,
                                per_page=per_page,
-                               pagination=pagination)
-        
-        # API Link accessing public data format https://storage.googleapis.com/BUCKET_NAME/OBJECT_NAME
-    else:
-        # redo temporary error response
-        return "Error Error"
+                               pagination=pagination,
+                               breed=breed, 
+                               species=species
+                               )
 
-# @bp.route('/profiles', methods=["GET", "POST"])
-# def view_profile():
-#     global species, breed, pdata
-#     if 'sub' not in session:
-#         return "sub not in session."
-#     else:
-#         if request.method == 'POST':
-#             content = request.get_json()
-#             species = content['species']
-#             breed = content['breed']
-#             if species == 'Any' and breed == "Any":
-#                 pdata = PetDsRepository.all()    
-#         else:
-#             if species == 'Any' and breed == "Any":
-#                 pdata = PetDsRepository.all() 
-#             else:
-#                 pdata = PetDsRepository.filter(species,breed)
-#         return render_template('profiles.html', pets = pdata,  breed = breed, species=species)
+        # return render_template('adopt_profiles.html', pets = pdata,  breed = breed, species=species)
+
+#added route to filter pets
+@bp.route('/filter', methods=["POST"])
+def filter():
+    global species, breed, pdata
+    content = request.get_json()
+    species = content['species']
+    breed = content['breed']
+    if species == 'Any' and breed == "Any":
+        pdata = PetDsRepository.all()
+    else:
+        pdata = PetDsRepository.filter(species,breed)
+    return render_template('adopt_profiles.html', pets = pdata)
