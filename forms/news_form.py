ce234970d727ab011c83b68699ab5ffdd2eec426
Amy Robertson <49350741+roberamy@users.noreply.github.com>
2021-02-25 16:15:09 -0800

    Forms updates

--- forms/news_form.py
+++ forms/news_form.py
@@ -1,7 +1,7 @@
 ###############################################################################
 #
-# Author: Gregory A. Bauer, Jasper Wong, Amy Robertson                                                        
-# Email: bauergr@oregonstate.edu                                                                              
+# Author: Gregory A. Bauer, Jasper Wong, Amy Robertson
+# Email: bauergr@oregonstate.edu
 # Course: CS467_400_W2021
 # Description: Form validation package used in 'news_add_edit' template.
 #   Creates form class with form fields and pertinent validation rules
@@ -10,14 +10,15 @@
 #
 ###############################################################################
 
-from wtforms import *
+from wtforms import StringField, TextAreaField, FileField, Form, validators
+
 
 class NewsForm(Form):
     ''' Form Field Options '''
     ''' End Form Field Options '''
     ''' Begin Form Validation Options '''
-    name = StringField('Enter Name', [validators.Length(min=3), validators.InputRequired()])
-    author = StringField('Author',[validators.Length(min=3), validators.InputRequired()])
-    content = TextAreaField('Content',[validators.Length(min=3), validators.InputRequired()])
+    title = StringField('Enter Name', [validators.Length(min=3), validators.InputRequired()])
+    author = StringField('Author', [validators.Length(min=3), validators.InputRequired()])
+    content = TextAreaField('Content', [validators.Length(min=3), validators.InputRequired()])
     profile_image = FileField('Image')
     ''' End Form Validation Options '''
